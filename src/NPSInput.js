91ed18faa51e6ebefbc1a65a5354e8677a180296
Samy Pessé <samypesse@gmail.com>
2017-02-25 18:31:24 +0100

    Handle message after submit

--- src/NPSInput.js
+++ src/NPSInput.js
@@ -50,8 +50,9 @@ const NPSInput = React.createClass({
     },
 
     render() {
-        const { dismissed } = this.state;
-        const { service } = this.props;
+        const { service, children } = this.props;
+        const { dismissed, score } = this.state;
+
         const message = service ?
             `How likely are you to recommend ${service} to your friends and colleagues?`
             : 'How likely are you to recommend us to your friends and colleagues?';
@@ -64,14 +65,19 @@ const NPSInput = React.createClass({
             <div className="NPSInput">
                 <button className="NPSInput-Close" onClick={this.onDismiss}>✕</button>
 
-                <div className="NPSInput-Inner">
-                    <p className="NPSInput-Message">
-                        {message}
-                    </p>
-                    <NPSScale
-                        onSubmit={this.onSubmit}
-                        />
-                </div>
+                {score ? (
+                    <div className="NPSInput-Inner">
+                        {children({ score })}
+                    </div>
+                ) : (
+                    <div className="NPSInput-Inner">
+                        <p className="NPSInput-Message">
+                            {message}
+                        </p>
+                        <NPSScale onSubmit={this.onSubmit} />
+                    </div>
+                )}
+
             </div>
         );
     }
