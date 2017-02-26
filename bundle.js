20fa5c7c63eed06d018f81fccafa9edfa3904f14
Samy Pessé <samypesse@gmail.com>
2017-02-26 01:13:05 +0100

    Updates

--- bundle.js
+++ bundle.js
@@ -20559,6 +20559,7 @@ module.exports = require('./lib/React');
 'use strict';
 
 var React = require('react');
+var classNames = require('classnames');
 var NPSScale = require('./NPSScale');
 
 /**
@@ -20569,6 +20570,7 @@ var NPSInput = React.createClass({
     displayName: 'NPSInput',
 
     propTypes: {
+        animated: React.PropTypes.bool,
         service: React.PropTypes.string,
         onSubmit: React.PropTypes.func.isRequired,
         onDismissed: React.PropTypes.func.isRequired,
@@ -20577,6 +20579,7 @@ var NPSInput = React.createClass({
 
     getDefaultProps: function getDefaultProps() {
         return {
+            animated: true,
             onSubmit: function onSubmit() {},
             onDismissed: function onDismissed() {},
             children: function children() {
@@ -20622,6 +20625,7 @@ var NPSInput = React.createClass({
     },
     render: function render() {
         var _props = this.props,
+            animated = _props.animated,
             service = _props.service,
             children = _props.children;
         var _state = this.state,
@@ -20637,7 +20641,7 @@ var NPSInput = React.createClass({
 
         return React.createElement(
             'div',
-            { className: 'NPSInput' },
+            { className: classNames('NPSInput', { animated: animated }) },
             React.createElement(
                 'button',
                 { className: 'NPSInput-Close', onClick: this.onDismiss },
@@ -20663,7 +20667,7 @@ var NPSInput = React.createClass({
 
 module.exports = NPSInput;
 
-},{"./NPSScale":181,"react":179}],181:[function(require,module,exports){
+},{"./NPSScale":181,"classnames":2,"react":179}],181:[function(require,module,exports){
 'use strict';
 
 var React = require('react');
@@ -20738,7 +20742,11 @@ var NPSScale = React.createClass({
                                 return _this.onSelect(i);
                             }
                         },
-                        React.createElement('div', null)
+                        React.createElement(
+                            'div',
+                            null,
+                            i
+                        )
                     );
                 })
             ),
